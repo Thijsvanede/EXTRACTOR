ab6abe9861ec0dd19bc9700709f0657f4ac0fc62
Thijs van Ede <t.s.vanede@utwente.nl>
2021-11-09 10:24:37 +0100

    Explicitly call packages from main

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from preprocessing   import *
-from role_generator  import *
-from graph_generator import *
-
-
+# Imports
 import argformat
 import argparse
+from nltk import sent_tokenize
+
+# Imports from package
+import preprocessing
+import role_generator
+import graph_generator
 
 # Parse arguments
 parser = argparse.ArgumentParser(
@@ -24,26 +26,26 @@ args = parser.parse_args()
 
 if __name__ == "__main__":
 
-    txt = modification_()
+    txt = preprocessing.modification_()
     txt = txt.strip()
-    txt = colon_seprator_multiplication(txt)
+    txt = role_generator.colon_seprator_multiplication(txt)
     txt = re.sub(' +', ' ', txt)
     sentences_ = sent_tokenize(txt)
-    lst = roles(sentences_)
-    lst = fix_srl_spacing(lst)
-    all_nodes = negation_clauses(lst)
+    lst = role_generator.roles(sentences_)
+    lst = role_generator.fix_srl_spacing(lst)
+    all_nodes = role_generator.negation_clauses(lst)
     if args.asterisk == 'true':
-        all_nodes = astriks(all_nodes)
-        all_nodes = triplet_builder(all_nodes)
+        all_nodes = role_generator.astriks(all_nodes)
+        all_nodes = role_generator.triplet_builder(all_nodes)
     else:
-        all_nodes = triplet_builder(all_nodes)
-    all_nodes = remove_no_sub(all_nodes)
-    lst = remove_c_colon_toprevent_graphvizbug(all_nodes)
+        all_nodes = role_generator.triplet_builder(all_nodes)
+    all_nodes = graph_generator.remove_no_sub(all_nodes)
+    lst = graph_generator.remove_c_colon_toprevent_graphvizbug(all_nodes)
     for i in lst:
         if "\\'" in i:
             i.replace("\\'", "'")
     if args.rmdup == "true":
-        lst = rm_duplictes(lst)
-        graph_builder(lst)
+        lst = graph_generator.rm_duplictes(lst)
+        graph_generator.graph_builder(lst)
     else:
-        graph_builder(lst)
+        graph_generator.graph_builder(lst)
